68e409ac928b94597b9eac11500a3cd6f76f5b97
Habiba Akhter <habibaakhter2012@gmail.com>
2019-12-06 13:57:06 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import TrainData1 from './modules/TrainData1.js'
+import TrainMoving from './modules/TrainMoving.js'
+
+let trainData1 = new TrainData1;
+trainData1.getTrainTime();
+
+new TrainMoving;
